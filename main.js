6e8c61e571e7940e1a4bc92e9494485cac71c1af
khanthulhu <johnlrbrock@gmail.com>
2017-10-27 23:42:51 -0400

    implemented dps

--- main.js
+++ main.js
@@ -2,14 +2,22 @@ new Vue({
   el: '#app',
   data: {
     count: 0,
+    dps: 0,
+    leftoverRespect: 0,
   },
   methods: {
     autoClicker: function autoClicker() {
-      let self = this;
-      self.count -= 100;
-      setInterval(function () {
-        self.count += 1;
-      }, 1000);
+      this.dps += 100;
+      console.log(this.dps);
     },
+  },
+  beforeMount() {
+    let self = this;
+    setInterval(function() {
+      let temp = (self.dps / 100) + self.leftoverRespect;
+      self.leftoverRespect = temp % 1;
+      temp -= self.leftoverRespect;
+      self.count += temp;
+    }, 10)
   },
 });
